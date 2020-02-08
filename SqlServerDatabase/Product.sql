13a0390e17dd51530c31ed2e5a158ba49a55fbcd
Dayal Sagar <dayalsagar8@gmail.com>
2020-02-08 08:16:27 +0530

    Added Code Column

--- SqlServerDatabase/Product.sql
+++ SqlServerDatabase/Product.sql
@@ -3,5 +3,6 @@
 	[Id] INT NOT NULL PRIMARY KEY, 
     [Name] NVARCHAR(50) NULL, 
     [Cost] INT NULL, 
-    [Color] NVARCHAR(50) NULL
+    [Color] NVARCHAR(50) NULL, 
+    [Code] NVARCHAR(50) NULL
 )
